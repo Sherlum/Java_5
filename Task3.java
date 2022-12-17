a321bf4929ea1dfdaf25e45e018527f39120416b
Stanislav <hansolo99@yandex.ru>
2022-12-17 20:38:21 +0300

    Final

--- Task3.java
+++ Task3.java
@@ -1,15 +1,33 @@
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Task3 {
 
-// Дана строка. Необходимо написать метод, который отсортирует слова в строке по длине с помощью TreeMap.
-// Слова с одинаковой длиной не должны “потеряться”.
-public static void main(final String[] args) {
+    public static void main(String[] args) {
 
-}
+        Map<String, String> unsortMap = new HashMap<String, String>();
+        unsortMap.put("0", "вы");
+        unsortMap.put("1", "об");
+        unsortMap.put("2", "вы");
+        unsortMap.put("3", "это");
+        unsortMap.put("4", "если");
+        unsortMap.put("5", "этом");
+        unsortMap.put("6", "можете");
+        unsortMap.put("7", "можете");
+        unsortMap.put("8", "мечтать");
+        unsortMap.put("9", "сделать");
+    
+    System.out.println("Если вы можете мечтать об этом вы можете это сделать\n");
 
-public String getSortedIncludesWords(final Map<String, String> map){
+        Map<String, String> treeMap = new TreeMap<String, String>(unsortMap);
+        printMap(treeMap);
+    }
 
-    return "";
-}
+    public static <K, V> void printMap(Map<K, V> map) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+
 }
